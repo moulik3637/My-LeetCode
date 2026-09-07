01a4833082e880c49128c2f83291486805435dad
Moulik Dotasara <moulikdotasara3637@gmail.com>
2026-09-07 22:08:31 +0530

    Time: 194 ms (5.10%) | Memory: 11.8 MB (16.12%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -27,7 +27,7 @@ bool fun(vector<int> &have, vector<int> &need)
         {
             have[s[high]]++;
 
-            while(fun(have,need)) // jab tk sahi hai
+            while(fun(have,need)) 
             {
                 int len=high-low+1;
                 if(res>len)
